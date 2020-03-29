66ba4df8451d0f847f07d5ab178e505e0361ca76
Tushar968 <tusharhavegalaxy@gmail.com>
2020-03-29 17:18:34 +0530

    Some minor fixes and updations

--- Question2.c
+++ Question2.c
@@ -3,15 +3,17 @@
 
 int main()
 {
-	int pipefds1[2], pipefds2[2];
-	int returnstatus1, returnstatus2;
+	int pipe1[2], pipe2[2];
+	int status1, status2;
 	int pid;
 	char message1[50];
 	char message2[50]={'\0'};
+	printf("Enter the string message:");
 	fgets(message1, 50, stdin);
 
 	char read_message[50];
 
+	//Changing the case of characters
 	int ascii=0;
 	for(int i=0; i<sizeof(message1); i++)
 	{
@@ -30,41 +32,44 @@ int main()
 			message2[i]=(char)ascii;
 	}
 
+	printf("Parent process with pid = %d and ppid = %d\n\n", getpid(), getppid());
 
-	returnstatus1= pipe(pipefds1);
-
-	if (returnstatus1==-1)
+	status1 = pipe(pipe1);
+	if (status1 == -1)
 	{
 		printf("Unable to create pipe 1\n");
 		return 1;
 	}
 
-	returnstatus2 = pipe(pipefds2);
-
-	if (returnstatus2==-1)
+	status2 = pipe(pipe2);
+	if (status2==-1)
 	{
-		printf("Unable to create pipe 2");
+		printf("Unable to create pipe 2\n");
 		return 1;
 	}
 
+	//Creating two processes
 	pid = fork();
+
 	if (pid!=0)
 		{
-			close(pipefds1[0]);
-			close(pipefds2[1]);
-			printf("In parent: Writing to pipe 1 - Message is %s\n", message1);
-			write(pipefds1[1], message1, sizeof(message1));
-			read(pipefds2[0], read_message, sizeof(read_message));
-			printf("In parent: Reading from pipe 2 - Message is %s\n", read_message);
+			//Parent Process (first process)
+			close(pipe1[0]);
+			close(pipe2[1]);
+			printf("Message sent through pipe 1 in first process (pid = %d and ppid = %d): %s\n", getpid(), getppid(), message1);
+			write(pipe1[1], message1, sizeof(message1));
+			read(pipe2[0], read_message, sizeof(read_message));
+			printf("Message received from pipe 2 in first process (pid = %d and ppid = %d): %s\n", getpid(), getppid(), read_message);
 		}
 	else
 		{
-			close(pipefds1[1]);
-			close(pipefds2[0]);
-			read( pipefds1[0], read_message, sizeof(read_message));
-			printf("In child: Reading from pipe 1 - Message is %s\n", read_message);
-			printf("In child: Writing to pipe 2 - Message is %s\n", message2);
-			write(pipefds2[1], message2, sizeof(message2));
+			//Child Process (Second process)
+			close(pipe1[1]);
+			close(pipe2[0]);
+			read( pipe1[0], read_message, sizeof(read_message));
+			printf("Message received from pipe 1 in second process (pid = %d and ppid=%d): %s\n", getpid(), getppid(), read_message);
+			printf("Message sent through pipe 2 in second process (pid = %d and ppid = %d): %s\n", getpid(), getppid(), message2);
+			write(pipe2[1], message2, sizeof(message2));
 		}
 	return 0;
 }
